5f2b24805ecbe768f20964ca4da21d69e5607feb
Gabriel Proença Alves <gproenca42@gmail.com>
2023-01-23 16:35:19 -0300

    Add ex vectors 03
    Close #3

---
+++ vetores/03/main.c
@@ -0,0 +1,28 @@
+/**
+ * Leia um vetor de 16 posições e troque os 8
+ * primeiros valores pelos 8 últimos e vice-e-versa.
+ * Escreva ao final o vetor obtido.
+ *
+ **/
+
+#include <stdio.h>
+
+int main(void)
+{
+        int vetor[16];
+
+        for (int i = 0; i < 16; i++) {
+                printf("vetor[%i] = ", i);
+                scanf("%i", &(vetor[i]));
+        }
+
+        for (int i = 0; i < 8; i++) {
+                vetor[i]   = vetor[i] ^ vetor[i+8];
+                vetor[i+8] = vetor[i] ^ vetor[i+8];
+                vetor[i]   = vetor[i] ^ vetor[i+8];
+        }
+
+        for (int i = 0; i < 16; i++)
+            printf("vetor[%i] = %i\n", i, vetor[i]);
+        return 0;
+}
