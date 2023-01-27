2c97681b3c578e1fd8d43b418185cc84f8f0c7e1
Gabriel Proença Alves <gproenca42@gmail.com>
2023-01-27 18:47:46 -0300

    Add ex vectors 08

---
+++ vetores/08/main.c
@@ -0,0 +1,27 @@
+/**
+ * leia um vetor de 40 posições e acumule os
+ * valores do primeiro elemento no segundo,
+ * deste no terceiro e assim por diante. Ao
+ * final, escreva o vetor obtido.
+ **/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int vetor[40];
+
+    for (int i = 0; i < 40; i++) {
+        printf("vetor[%2i] = ", i);
+        scanf("%i", &(vetor[i]));
+    }
+
+    printf("vetor[ 0] = %i\n", vetor[0]);
+
+    for (int i = 1; i < 40; i++) {
+        vetor[i] += vetor[i-1];
+        printf("vetor[%2i] = %i", i, vetor[i]);
+    }
+
+    return 0;
+}
